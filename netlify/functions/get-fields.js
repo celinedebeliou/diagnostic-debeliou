6663e670e9ba8e0bee51e42e64ca945177041d3e
celinedebeliou <celine@debeliou.com>
2026-08-11 16:04:31 -0400

    Add files via upload

--- netlify/functions/get-fields.js
+++ netlify/functions/get-fields.js
@@ -1,16 +1,33 @@
 const SYSTEME_API_KEY = "bso261tpln7ilky2mdgx8oqvd7x3px74y0h8o0lmv7zr4widg8cvjybz5gg2a3bz";
 
 exports.handler = async () => {
-  const res = await fetch("https://api.systeme.io/api/contact-fields", {
-    headers: {
-      "X-API-Key": SYSTEME_API_KEY,
-      "Content-Type": "application/json"
-    }
-  });
-  const data = await res.json();
-  return {
-    statusCode: 200,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    body: JSON.stringify(data, null, 2)
-  };
+  try {
+    const res = await fetch("https://api.systeme.io/api/contacts?limit=1", {
+      headers: {
+        "X-API-Key": SYSTEME_API_KEY,
+        "Content-Type": "application/json"
+      }
+    });
+
+    const text = await res.text();
+
+    // Essaie de parser le premier contact pour voir ses champs
+    let data;
+    try { data = JSON.parse(text); } catch(e) { data = { raw: text }; }
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data, null, 2)
+    };
+  } catch(err) {
+    return {
+      statusCode: 500,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      body: JSON.stringify({ error: err.message })
+    };
+  }
 };
